6fadf18cc589e51441000fd1f93e5cba17dad05f
imylonaxx <ylona.fabiani@etu.umontpellier.fr>
2020-11-03 23:12:04 +0100

    j'ai fait groso modo les scene, manque les cases à texte dans la grille => chercher à le faire en java et pas en fx, faire fonction des boutons

--- src/sample/SudokuController.java
+++ src/sample/SudokuController.java
@@ -1,9 +1,12 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class SudokuController {
     //déclaration de la scène
@@ -21,5 +24,12 @@ public class SudokuController {
     //BoutonLancer : au click => lancerPartie()
     //BoutonQuitter : au click => quitterJeu()   (fermer la fenêtre)
 
+    public void lancerPartie(ActionEvent event) throws IOException {
+        //fonction lancer la partie
+    }
+
+    public void quitterJeu(ActionEvent event) throws  IOException {
+        //fonction quitter le jeu
+    }
 
 }
