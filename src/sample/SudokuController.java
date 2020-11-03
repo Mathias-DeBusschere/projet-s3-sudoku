a5775d7e0ce2506ccb35d87a00b7250f23247133
imylonaxx <ylona.fabiani@etu.umontpellier.fr>
2020-11-03 13:09:31 +0100

    j'ai avancé

--- src/sample/SudokuController.java
+++ src/sample/SudokuController.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 public class SudokuController {
+    //déclaration de la scène
+    private Scene sceneSudoku;
+    public void setSceneSudoku(Scene scene) {
+        sceneSudoku = scene;
+    }
+    //déclaration des "variables" utilisées
+    @FXML
+    private Button lancerPartie;
+    @FXML
+    private Button quitterJeu;
+
+
+    //BoutonLancer : au click => lancerPartie()
+    //BoutonQuitter : au click => quitterJeu()   (fermer la fenêtre)
+
+
 }
