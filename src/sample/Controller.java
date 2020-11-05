1656e0da7a1f0b06d3575b2235cdb413555b15c4
ylow <ylona.fabiani@etu.umontpellier.fr>
2020-11-05 19:15:46 +0100

    grille lancée

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -29,24 +30,22 @@ public class Controller extends Parent {
     //BoutonQuitter : au click => quitterJeu()   (fermer la fenêtre)
     @FXML
     public void lancerPartie(MouseEvent event) throws IOException {
-        Parent grille = FXMLLoader.load(getClass().getResource("Grille.fxml"));
+        /*Parent grille = FXMLLoader.load(getClass().getResource("Grille.fxml"));
         //getScene(new Scene(grille, 860, 700));
-        ((Stage) getScene().getWindow()).setScene(new Scene(grille,860,700));
-    }
+        ((Stage) getScene().getWindow()).setScene(new Scene(grille,860,700));*/
 
 
-    /*void actionFlecheRetour2(MouseEvent event) throws IOException {
-        FXMLLoader cabine2Loader = new FXMLLoader(getClass().getResource("Cabine2.fxml"));
-        Parent firstPane = cabine2Loader.load();
+        FXMLLoader grilleLoader = new FXMLLoader(getClass().getResource("Grille.fxml"));
+        Parent firstPane = grilleLoader.load();
 
-        Scene sceneCabine2 = new Scene(firstPane, 1280, 720);
+        Scene sceneGrille = new Scene(firstPane, 860, 700);
 
-        Cabine2Controller cabine2Controller = (Cabine2Controller) cabine2Loader.getController();
-        cabine2Controller.setSceneCabine2(((Node)event.getSource()).getScene());
+        Controller controller = (Controller) grilleLoader.getController();
+        controller.setScene(((Node)event.getSource()).getScene());
 
         Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        primaryStage.setScene(sceneCabine2);
-    }*/
+        primaryStage.setScene(sceneGrille);
+    }
 
     public void fermerJeu(MouseEvent event) throws  IOException {
         Platform.exit();
