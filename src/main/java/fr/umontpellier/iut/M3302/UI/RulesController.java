2ace6d55d670d0a534f4dca2fa2274536410ee87
ylow <ylona.fabiani@etu.umontpellier.fr>
2021-01-10 15:12:19 +0100

    rules.fxml ajout des bases

--- src/main/java/fr/umontpellier/iut/M3302/UI/RulesController.java
+++ src/main/java/fr/umontpellier/iut/M3302/UI/RulesController.java
@@ -1,4 +1,20 @@
 package fr.umontpellier.iut.M3302.UI;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RulesController {
+
+    /*@FXML
+    private void rules(MouseEvent event) throws IOException {
+        Parent rulesLoad = FXMLLoader.load(getClass().getResource("/fxml/Rules.fxml"));
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.getScene().setRoot(rulesLoad);
+    }*/
 }
