b91f1999703c3fad13ac2ecbe28192be2e345bb9
ylow <ylona.fabiani@etu.umontpellier.fr>
2021-01-10 16:27:14 +0100

    rules + rules controller DONE

--- src/main/java/fr/umontpellier/iut/M3302/UI/RulesController.java
+++ src/main/java/fr/umontpellier/iut/M3302/UI/RulesController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -11,10 +12,12 @@ import java.io.IOException;
 
 public class RulesController {
 
-    /*@FXML
-    private void rules(MouseEvent event) throws IOException {
-        Parent rulesLoad = FXMLLoader.load(getClass().getResource("/fxml/Rules.fxml"));
+    @FXML
+    public Button mainMenu;
+    @FXML
+    private void mainMenu(MouseEvent event) throws IOException {
+        Parent mainMenuLoad = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        primaryStage.getScene().setRoot(rulesLoad);
-    }*/
+        primaryStage.getScene().setRoot(mainMenuLoad);
+    }
 }
